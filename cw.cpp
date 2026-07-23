0eabc04f9c23769674adf20e03a1a52938047712
icaro <icaro@localhost.localdomain>
2026-07-23 15:16:46 +0200

    70%

--- cw.cpp
+++ cw.cpp
@@ -76,7 +76,7 @@ const char* letreAleatorie(int intervale = 26) { //26 lettres o 36 lettres + nom
 
 //ton d'erreur quand on fait une erreur
 void erreurTone(){
-	tone(AUDIO, 100);delay(300); noTone(AUDIO);
+	tone(AUDIO, 100);delay(1000); noTone(AUDIO);
 }
 
 //La mélodie de Mario Bros quand on a gagné
@@ -137,10 +137,13 @@ void jeuCW1 (KeyboardUSB& cw,int& mpm, int& freq, int& menu){
   }
 
     if (cw.isBuffer()){//Une touche a été appuyée ?
+    
       printLcd (9,1,"        ");
 
       c=cw.getBuffer()[0];//Get touche
       cw.clearBuffer();
+
+      if (c==0x28){c='\0';}//ENTER
 
       //MPM MENU FREQ
       if (c=='&'){mpm++;c = '\0'; setUpCw(mpm, freq);} //1 &
@@ -148,6 +151,9 @@ void jeuCW1 (KeyboardUSB& cw,int& mpm, int& freq, int& menu){
 
       if (c==35){menu++;c = '\0';} //3 #
       if (c==123){menu--;c = '\0';} //4 {  
+
+      if (menu<0 ){menu=0;}
+      if (menu>1 ){menu=1;}
 
       if (c==40){freq+=10;c = '\0'; setUpCw(mpm, freq);} //5 (
       if (c==45){freq-=10;c = '\0'; setUpCw(mpm, freq);} //6 -
@@ -162,11 +168,12 @@ void jeuCW1 (KeyboardUSB& cw,int& mpm, int& freq, int& menu){
         correctes++;
 
       }else if (c!='\0'){
-        printLcd (10,1,"NON !");
+        //printLcd (10,1,"NON !");
+        printLcd (10,1,String(c)+" ");
         erreurTone();
         printLcd (10,1,"      ");
 
-        playCW  (getCwCode(lettre));//Erreur rePlay CW 
+       // playCW  (getCwCode(lettre));//Erreur rePlay CW 
         incorrectes++;       
         nouvelle =true;//nouvelle lettre aleatoire
       }
@@ -175,7 +182,7 @@ void jeuCW1 (KeyboardUSB& cw,int& mpm, int& freq, int& menu){
         printLcd (2,1,String (correctes)+":"+String (incorrectes)+"  ");
 
         if (totale>=MAX_JEU ){//A atteint le nombre maximal de lettres ?
-          if ((correctes*100.0/totale)>=90){
+          if ((correctes*100.0/totale)>=70){
             printLcd (0,1,"VOUS AVEZ GAGNE!");
             playMarioVictoryMelody();//Winner
           }else{
